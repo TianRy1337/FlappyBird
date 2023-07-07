c64cbf63b635b35e3a2687090449ca6055a9c539
ZhongYaoZhang <Ryan55014TianZhang@gmail.com>
2023-07-07 23:47:24 +0800

    更新專案版本、調整專案結構
    重新命名檔案、移除重複不必要資源、忽略字體檔

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void GameDelegate();//設定一個叫GameDelegate 的委派(delegate)
+    public static event GameDelegate OnGameStarted;//在GameDelegate內設置一個開始遊戲的靜態事件
+    public static event GameDelegate OnGameOverConfirmed;//在GameDelegate內設置一個遊戲結束確定的靜態事件
+
+    public static GameManager Instance;//創造一個靜態可及性參考，讓其他Scripts從這個Instance進來這個Class
+    public GameObject startPage;//開始畫面
+    public GameObject gameoverPage;//遊戲結束
+    public GameObject countdownPage;//倒數
+    public Text scoreText;//分數>>記得用UnityEngine.UI
+    enum PageState
+    {//總共會有這四種頁面
+        None,
+        Start,
+        GameOver,
+        CountDown
+    }
+
+    int score = 0;//儲存分數 
+    bool gameOver = true;//給一個bool值用來確定遊戲是否結束
+    public bool GameOver { get { return gameOver; } }//製造一個可用但無法被其他Script修改的變數
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
+        CountdwonText.OnCountdownFinished += OnCountdownFinished;
+        TapController.OnPlayerDied += OnPlayerDied;
+        TapController.OnPlayerScored += OnPlayerScored;
+    }
+
+    void OnDisable()
+    {
+        CountdwonText.OnCountdownFinished -= OnCountdownFinished;
+        TapController.OnPlayerDied -= OnPlayerDied;
+        TapController.OnPlayerScored -= OnPlayerScored;
+    }
+
+    void OnCountdownFinished()
+    {
+        SetPageState(PageState.None);
+        OnGameStarted();//event snet to TapController
+        score = 0;
+        gameOver = false;
+    }
+
+    void OnPlayerDied()
+    {
+        gameOver = true;
+        int savedScored = PlayerPrefs.GetInt("HighScore");
+        if (score > savedScored)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+        SetPageState(PageState.GameOver);
+    }
+
+    void OnPlayerScored()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+
+    void SetPageState(PageState state)
+    {//頁面確認及轉換
+        switch (state)
+        {
+            case PageState.None:
+                startPage.SetActive(false);
+                gameoverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameoverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameoverPage.SetActive(true);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.CountDown:
+                startPage.SetActive(false);
+                gameoverPage.SetActive(false);
+                countdownPage.SetActive(true);
+                break;
+        }
+    }
+    public void ConfirmGameOver()
+    {
+        //重玩被按下的時候>被附加在重玩鈕上
+        OnGameOverConfirmed();//event snet to tapcontroller
+        scoreText.text = "0";
+        SetPageState(PageState.Start);
+    }
+    public void StartGame()//開始遊戲>被附加在開始遊戲鈕上
+    {
+        SetPageState(PageState.CountDown);
+    }
+
+}
---
+++ Assets/Scripts/TapController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+//RequireComponent指這個類一定要那些組件，沒有這些組件沒有被加上，就自動補上
+public class TapController : MonoBehaviour
+{
+    public delegate void PlayerDelegate(); //設定一個叫PlayerDelegate 的委派(delegate)
+    public static event PlayerDelegate OnPlayerDied;//在PlayerDelegate內設置一個死亡的靜態事件
+    public static event PlayerDelegate OnPlayerScored;//在PlayerDelegate內設置一個得分的靜態事件
+
+    public float tapForce = 10;//點擊力量
+    public float tiltSmooth = 5;//傾斜時的平滑
+    public Vector3 startPos;//設置三維座標 叫 starPos
+    Rigidbody2D newrigidbody;//設置一個Rigidbody變數叫newridigbody
+    Quaternion downRotation;//設置一個四元數的變數叫downRotation
+    Quaternion forwardRotation;//設置一個四元數的變數叫forwarRotation
+
+    GameManager game;
+
+    public void Start()
+    {//遊戲開始時執行
+        newrigidbody = GetComponent<Rigidbody2D>();//抓到該物件的Rigidbody2D ,回傳附加型態，如果沒有就傳Null
+        downRotation = Quaternion.Euler(0, 0, -90);//返回圍繞z軸-90的旋轉
+        forwardRotation = Quaternion.Euler(0, 0, 35);//返回圍繞z軸35的旋轉
+        game = GameManager.Instance;
+        newrigidbody.simulated = false;//關閉該物件的Rigidbody2D
+    }
+
+
+    void OnEnable()
+    {
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+    void OnDisable()
+    {
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameStarted()
+    {
+        newrigidbody.velocity = Vector3.zero;
+        newrigidbody.simulated = true;
+    }
+
+
+    void OnGameOverConfirmed()
+    {
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
+    }
+
+    public void FixedUpdate()
+    {
+        if (game.GameOver)
+            return;
+        if (Input.GetMouseButton(0))
+        {//如果按下滑鼠左鍵 0=左鍵 1=右鍵 2=中鍵
+            transform.rotation = forwardRotation;//每次點擊鳥的弧度回到原位
+            newrigidbody.velocity = Vector3.zero;//將速度重新為0 否則重力速度會大於點擊速度
+            newrigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);//每次點擊都能向上
+
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);//鳥沿著Z軸迴轉的滑順度
+    }
+    //設置一個Function負責判斷是否擊中Scorezone跟DeadZone
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "ScoreZone")
+        {
+            //計分
+            OnPlayerScored();// event sent to GameManager;
+        }
+        if (col.gameObject.tag == "DeadZone")
+        {
+            newrigidbody.simulated = false;//關閉碰撞
+            OnPlayerDied();
+
+        }
+    }
+
+}
